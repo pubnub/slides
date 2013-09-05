3369cb53f0b039d605810795b34e655b9a1bd66f
dristic <dristic101@gmail.com>
2013-09-05 13:45:48 -0700

    Add controller page for presentations.

---
+++ controller/controller.js
@@ -0,0 +1,46 @@
+(function () {
+  var pubnub = null;
+
+  var next = document.querySelector("#next"),
+      previous = document.querySelector("#previous"),
+      pubKey = document.querySelector("#publish-key");
+
+  function initPubNub() {
+    if (!pubnub) {
+      pubnub = PUBNUB.init({
+        publish_key: pubKey.value,
+        subscribe_key: 'sub-c-4c782460-ced1-11e2-9fea-02ee2ddab7fe'
+      });
+    }
+  };
+
+  // Next click handler
+  next.addEventListener('click', function (event) {
+    initPubNub();
+
+    var message = {
+      command: 'next'
+    };
+    message = JSON.stringify(message);
+
+    pubnub.publish({
+      channel: 'slides',
+      message: message
+    });
+  });
+
+  // Previous click handler
+  previous.addEventListener('click', function (event) {
+    initPubNub();
+
+    var message = {
+      command: 'previous'
+    };
+    message = JSON.stringify(message);
+
+    pubnub.publish({
+      channel: 'slides',
+      message: message
+    });
+  });
+})();
